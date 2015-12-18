873d7319e775e8bddb9fd17429ea9b9fe293e88c
Ben Cook <b@bencook.ca>
2015-12-18 10:36:04 -0800

    getting reasonably speedy

--- src/blx/sortable.clj
+++ src/blx/sortable.clj
@@ -31,90 +31,66 @@
     (->> (line-seq rdr)
          (mapv parse-json))))
 
-(defn remove-special-chars [s]
-  ((fnil str/replace "") s #"[_-]" ""))
-
-(defn remove-spaces [s]
-  ((fnil str/replace "") s #" " ""))
+(defn remove-chars [s]
+  (-> s
+      ((fnil str/replace "") #"[ _-]+" "")
+      str/lower-case))
 
 (defn prepare-product [product]
   (-> product
-      (update :product_name (comp remove-spaces remove-special-chars))))
-
+      (update :product_name remove-chars)
+      (update :model remove-chars)))
 
 
 (defn longest-starting-match [a b]
-  (->> [a b]
-       (map (comp remove-spaces remove-special-chars))
-       (map str/lower-case)
-       (apply map vector)
-       (reduce (fn [match-len [a b]]
-                 (if (= a b)
-                   (inc match-len)
-                   (reduced match-len)))
-               0)))
-
+  (reduce (fn [match-len [a b]]
+            (if (= a b)
+              (inc match-len)
+              (reduced match-len)))
+          0
+          (map vector a b)))
 
 
 
 (defn match-manufacturer [mproducts listing]
   (->> (keys mproducts)
-       (some #(when (.startsWith (:manufacturer listing) %)
+       (map remove-chars)
+       (some #(when (.startsWith (remove-chars (:manufacturer listing)) %)
                 %))))
 
 (defn match-title [products listing]
-  (let [min-match-len 3
+        ; Longer than just manufacturer name
+  (let [min-match-len (max 3 (count (:manufacturer listing)))
         matches (->> products
                      (map #(vector (longest-starting-match (:title listing)
                                                            (:product_name %))
                                    %))
-                     (sort-by first >)
-                     (filter (fn [[_ prod]]
-                               (if (:model prod)
-                                 (.contains (remove-special-chars (remove-spaces (:title listing)))
-                                            (remove-special-chars (remove-spaces (:model prod))))
-                                 true)))
-                     (filter #(> (first %)
-                                 ; Longer than just manufacturer name
-                                 (max min-match-len (count (:manufacturer listing)))))
-                     (take 3))]
+                     (filter (fn [[score prod]]
+                               (and (> score min-match-len)
+                                    (if-let [model (:model prod)]
+                                      (.contains (:title listing) model)
+                                      true))))
+                     (sort-by first >))]
     ; Do not allow ties
     (when (and matches
                (not= (first (first matches))
                      (first (second matches))))
       (first matches))))
 
-(defn match' [products listing]
-  (println "Trying to match " listing)
-  (let [mproducts (group-by :manufacturer products)
-        est-manufacturer (match-manufacturer mproducts listing)
-        est-products (match-title products listing)]
-    [est-manufacturer est-products]))
+(defn match [products listing]
+  (let [listing' (update listing :title remove-chars)]
+    (when-let [[_ match] (match-title products listing')]
+      [listing match])))
 
-
-(def match-threshold 20)
-
-(def match-preds
-  [[(fn [products listing]
-      (some #(.startsWith (:manufacturer listing) %)
-            (map :manufacturer products)))
-    20]])
-
-
-(defn match-product
-  "Returns the product matched by listing, or nil if no match."
-  [products listing]
-  (let [score-seq (map (fn [[pred weight]]
-                         (when (pred listing)
-                           weight))
-                       match-preds)]
-    (reduce (fn [score x]
-              (let [score' (+ score x)]
-                (if (>= score' match-threshold)
-                  (reduced score')
-                  score')))
-            0 score-seq)))
-
+(defn domatch [products listings]
+  (let [matches (->> listings
+                     (pmap #(match products %))  ; pmap was a 4x speedup
+                     (filter some?))]
+    (->> matches
+         (group-by second)
+         (map (fn [[prod matching-listings]]
+                {:product_name (:product_name prod)
+                 :listings (map first matching-listings)})))))
 
 
 (defn -main
