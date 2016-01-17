0f1222bd3165a9eae00a080816bdee2d1867cef2
Ben Cook <b@bencook.ca>
2016-01-16 21:13:50 -0800

    add description to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject blx.sortable "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Solution to the Sortable coding challenge. Attempts to match product listings with a known set of products."
+  :url "https://github.com/blx/sortable-challenge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
