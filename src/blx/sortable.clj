76dd25083fd2a6420e2708712f6c66eb00fbb854
Ben Cook <b@bencook.ca>
2016-01-16 18:59:34 -0800

    add some docstrings, rename some vars

--- src/blx/sortable.clj
+++ src/blx/sortable.clj
@@ -23,76 +23,80 @@
 ; Result :: {:product_name str
 ;            :listings [Listing]}
 
-(defn- parse-json [s]
+(defn- parse-json
+  "Parses a JSON string, keywordizing keys."
+  [s]
   (json/parse-string s true))
 
-(defn load-input [path]
+(defn load-input
+  "Parses the file at `path` as a vector of JSON objects."
+  [path]
   (with-open [rdr (io/reader path)]
     (->> (line-seq rdr)
+         ; We need to return a vector (mapv), because otherwise with-open will
+         ; close the stream before the lazy sequence is realized.
          (mapv parse-json))))
 
-(defn remove-chars [s]
-  (-> s
-      ((fnil str/replace "") #"[ _-]+" "")
+(defn normalize-str [s]
+  (-> (or s "")
+      (str/replace #"[ _-]+" "")
       str/lower-case))
 
 (defn prepare-product [product]
   (-> product
-      (update :product_name remove-chars)
-      (update :model remove-chars)))
-
+      (update :product_name normalize-str)
+      (update :model normalize-str)))
 
 (defn common-prefix-length
-  "Returns the length of the longest common prefix between a and b."
+  "Returns the length of the longest common prefix between sequences a and b."
   [a b]
-  (reduce (fn [match-len [a b]]
-            (if (= a b)
-              (inc match-len)
-              (reduced match-len)))
-          0
-          (map vector a b)))
+  (->> (map = a b)
+       (take-while true?)
+       count))
 
-
-
-(defn match-manufacturer [mproducts listing]
-  (->> (keys mproducts)
-       (map remove-chars)
-       (some #(when (.startsWith (remove-chars (:manufacturer listing)) %)
-                %))))
+(comment
+  (defn match-manufacturer [mproducts listing]
+    (->> (keys mproducts)
+         (map remove-chars)
+         (some #(when (.startsWith (normalize-str (:manufacturer listing)) %)
+                  %))))
+)
 
 (defn match-title
   "Attempts to match listing to a product in products, returning a vector
-  [score matching-product] if successful, else nil."
+  [listing matching-product] if successful, else nil."
   [products listing]
+  (let [listing-title (normalize-str (:title listing))
         ; Longer than just manufacturer name
-  (let [min-match-len (max 3 (count (:manufacturer listing)))
+        min-match-len (max 3 (count (:manufacturer listing)))
+                     ; This is fairly fast. Combining the map and filter
+                     ; as one reduce into a (transient []) didn't help much
+                     ; and was harder to read.
         matches (->> products
-                     (map #(vector (common-prefix-length (:title listing)
+                     (map #(vector (common-prefix-length listing-title
                                                          (:product_name %))
                                    %))
                      (filter (fn [[score prod]]
                                (and (> score min-match-len)
                                     (if-let [model (:model prod)]
-                                      (.contains (:title listing) model)
+                                      (.contains listing-title model)
                                       true))))
                      (sort-by first >))]
     ; Do not allow ties
     (when (and matches
                (not= (first (first matches))
                      (first (second matches))))
-      (first matches))))
+      (let [[_ match] (first matches)]
+       [listing match]))))
 
-(defn match [products listing]
-  (let [listing' (update listing :title remove-chars)]
-    (when-let [[_ match] (match-title products listing')]
-      [listing match])))
-
-(defn domatch
+(defn match-all
   "Returns the sequence of Result maps produced by matching the
   listings to the products."
+  ; We take in all the listings, as opposed to doing (pmap match-listing listings),
+  ; because we need to group the results by Product after matching.
   [products listings]
   (let [matches (->> listings
-                     (pmap #(match products %))  ; pmap was a 4x speedup
+                     (pmap #(match-title products %))  ; pmap was a 4x speedup
                      (filter some?))]
     (->> matches
          (group-by second)
@@ -103,20 +107,23 @@
 
 
 (defn test-random [products listings]
-  ((juxt #(match products %) identity)
-   (rand-nth listings)))
+  (let [listing (rand-nth listings)]
+    (or (match-title products listing)
+        [nil listing])))
 
 
 
 (defn -main
   [& args]
-  ; We need to read all products into memory, but we
+  ; We need to read all products into memory at once, but we
   ; can process listings lazily / line-by-line.
-  (let [products (mapv prepare-product (load-input (:products-uri conf)))]
+  (let [products (map prepare-product (load-input (:products-uri conf)))]
     (with-open [listings (io/reader (:listings-uri conf))
-                out (io/writer (:out-uri conf))]
+                out      (io/writer (:out-uri conf))]
       (doseq [item (->> (line-seq listings)
                         (map parse-json)
-                        (domatch products))]
+                        (match-all products))]
         (json/generate-stream item out)
-        (.write out "\n")))))
+        (.write out "\n"))))
+  ; Put pmap to bed
+  (shutdown-agents))
